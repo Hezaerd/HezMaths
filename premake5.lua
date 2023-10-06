ef754ff3e1a753a0aeda75747305b99ee75dfb02
Swann <goldenland046@gmail.com>
2023-10-06 14:13:55 +0200

    push base project

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "HezMaths"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"HezMaths/**.hpp",
+		"HezMaths/**.cpp"
+	}
+
+	includedirs
+	{
+		"HezMaths"
+	}
+
+    warnings "Extra"
+
+	filter "system:windows"
+		systemversion "latest"
+
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
